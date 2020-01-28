72743bf2a7aa95eba2eb7fc86344d44d5004235f
William Richman <WRichma@mesaaz.gov>
2020-01-28 16:45:44 -0700

    Not sure just pushing going to class now

--- libs/Employee.js
+++ libs/Employee.js
@@ -10,32 +10,41 @@ methods:
   * getRole() // Returns 'Employee' */
 
 
+//for inquirer UA Coding Bootcamp 01.23.20 9.3 time stamp 2:04:50
+
+
 const inquirer = require('inquirer')
 
 inquirer
   .prompt({
-    message: "Enter Your Name Please ",
+    message: "Enter Your Name",
     name: "name"
   })
   .then(function (name) {
     console.log(name)
-    console.log(id)
   })
 
 inquirer
   .prompt({
-    message: "Enter Your ID Number ",
+    message: "Enter Your ID Number",
     id: "id"
   })
   .then(function (id) {
     console.log(id)
   })
 
+inquirer
+  .prompt({
+    message: "Enter Your Title",
+    title: "title"
+  })
+  .then(function (Title) {
+    console.log(title)
+  })
 
 const newEmp = {
   name: "",
   id: "",
   title: "",
-
 }
 
